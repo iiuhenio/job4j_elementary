059e33e4609eacd5bbdd739e30addae9cc7dfd51
User <iuheniomail@yandex.ru>
2020-04-13 19:39:48 +0300

    3.2. Разработка первой программы. [#272321]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Евгений");
+        System.out.println("06.08.1985");
+    }
+}
+
