b0216a49389acaff1d18cd45bd857a342fc0ed34
User <iuheniomail@yandex.ru>
2021-01-12 19:32:23 +0300

    3.1. Загрузка файлов на github[243562#272328] new

---
+++ src/ru/job4j/StudentInfo3.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo3 {
+    public static void main(String[] args) {
+        System.out.println("Evgeni programmer");
+        System.out.println("06/08/85");
+    }
+}
+
