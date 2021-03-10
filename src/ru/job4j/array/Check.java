7fdf7fefc9c458238c0c6b31e9c16d9c0c78922c
User <iuheniomail@yandex.ru>
2021-03-10 13:31:44 +0300

    6.4.1. Слово заканчивается на ...[156317#272282]

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -3,18 +3,12 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        for (int index = 0; index < data.length; index++) {
-            if (data[index] = data[0]) {
+        for (int index = 0; index < data.length - 1; index++) {
+            if (data[index] != data[0]) {
                 return false;
-            } else {
-                return true;
             }
-
-            }
-            return result;
-            /*        for () { */
-            /*        }        */
-
         }
-
+        return result;
+        }
     }
+
