3e814b9b62e0f82d70b9f15dee91ca985644b31a
User <iuheniomail@yandex.ru>
2021-03-10 11:52:31 +0300

    6.4. Слово начинается с ...[41585#272278]

---
+++ src/ru/job4j/array/Check.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] = data[0]) {
+                return false;
+            } else {
+                return true;
+            }
+
+            }
+            return result;
+            /*        for () { */
+            /*        }        */
+
+        }
+
+    }
