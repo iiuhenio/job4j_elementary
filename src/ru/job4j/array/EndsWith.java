53388d26eafa14bc835ea7d41047029862743379
User <iuheniomail@yandex.ru>
2021-03-11 20:06:56 +0300

    6.4.1. Слово заканчивается на ...[156317#272282]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        for (int index = 0; index <= word.length - 1; index++) {
-            if (word[word.length - 1] != post[1] || word[word.length - 2] != post[0]) {
+        for (int index = 0; index < word.length; index++) {
+            if (word.length - 1 != post[1] || word[word.length - 2] != post[0]) {
                 return false;
             }
 
