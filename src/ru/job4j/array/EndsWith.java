33642fc65160372f8889b1de1911b8d5ef47bf68
User <iuheniomail@yandex.ru>
2021-03-12 17:54:49 +0300

    6.5.1. Поиск минимального числа в массиве.[179803#272285]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        for (int index = 0; index < word.length; index++) {
-            if (word.length - 1 != post[1] || word[word.length - 2] != post[0]) {
+        for (int index = 0; index < post.length; index++) {
+            if (word[word.length - 1 - index] != post[post.length - 1 - index]) {
                 return false;
             }
 
