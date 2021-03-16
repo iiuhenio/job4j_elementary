b069ffeb4c94d3fc005a22f24fa54c7f35905cc1
User <iuheniomail@yandex.ru>
2021-03-16 19:36:11 +0300

    6.8. Дефрагментация массива.[225#272297]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -34,7 +34,9 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
-            if ((monoHorizontal(board, i) || monoVertical(board, i))) {
+            if (board[i][i] == 'X') {
+                result = true;
+            } else if (monoHorizontal(board, i) || monoVertical(board, i)) {
                 result = true;
                 break;
             }
--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -4,8 +4,8 @@ public class SortSelected {
 
     public static int[] sort(int[] data) {
         for (int index = 0; index < data.length; index++) {
-            int min = MinDiapason.findMin(data, 0, data.length); /* ищет мин.число в диап-не */
-            int index1 = FindLoop.indexOf(data, min, 0, data.length); /* ищет индекс мин.числа */
+            int min = MinDiapason.findMin(data, i, data.length - 1); /* ищет мин.число в диап-не */
+            int index1 = FindLoop.indexOf(data, min, i, data.length); /* ищет индекс мин.числа */
             int temp = data[index]; /* временная ячейка для хранения индекса 0 (например)  */
             data[index] = data[index1]; /* мин.число перемещаем в начало */
             data[index1] = temp; /* возращаем число из 0 на место минимального */
