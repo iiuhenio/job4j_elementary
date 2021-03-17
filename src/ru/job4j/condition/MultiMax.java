e247bb281401c2d2bda1cd7638abe6501e5fb2c9
User <iuheniomail@yandex.ru>
2021-03-17 15:04:40 +0300

    5.0. Цикл for. Определение.[3572#272360]

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -1,7 +1,7 @@
 package ru.job4j.condition;
 
 public class MultiMax {
-    public int max(int first, int second, int third) {
+    public static int max(int first, int second, int third) {
         int result = first >= second ? first : second;
         int result2 = result >= third ? result : third;
         return result2;
