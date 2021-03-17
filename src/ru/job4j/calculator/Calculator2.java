e33b7d46dc91d350eea9f9e2902034d3fdb24889
User <iuheniomail@yandex.ru>
2021-03-17 14:34:24 +0300

    6.5.3. Сортировка выборкой.[156320#272284]

--- src/ru/job4j/calculator/Calculator2.java
+++ src/ru/job4j/calculator/Calculator2.java
@@ -1,12 +1,10 @@
 package ru.job4j.calculator;
 
 public class Calculator2 {
-
     public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
     }
-
     public static void main(String[] args) {
         Calculator.plus(100, 500);
         Calculator.plus(4, 2);
