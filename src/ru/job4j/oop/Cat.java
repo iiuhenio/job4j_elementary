075bb86a39ce466ce145ae7922b1a666e8f737a4
User <iuheniomail@yandex.ru>
2021-04-05 20:41:28 +0300

    1.5. Поля объекта[166083#272439]

--- src/ru/job4j/oop/Cat.java
+++ src/ru/job4j/oop/Cat.java
@@ -1,8 +1,36 @@
 package ru.job4j.oop;
 
 public class Cat {
-    public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+    private String food; /* создали поле класса Кат - фуд */
+    private String name; /* 1. создали поле класс Кат - нэйм */
+
+    public void show() { /* выводит на консоль содержимое поля фуд */
+        System.out.println(this.food);  /* пишем this выводим содержимое поля */
+        System.out.println(this.name);
+
     }
-}
+
+    public void eat(String meat) {    /* записываем в поле объекта данные */
+        this.food = meat;             /* Выше приведенная строчка записывает в поле объект food значение переменной
+                                            meat. Значение переменной meat присваивается в методе main: */
+    }
+    public void giveNick(String nick) {    /* 2 */
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        System.out.print("съел ");
+        gav.eat("kotleta");                                                     /* объект класса Cat gav*/
+        gav.show();                                                    /*   Значение переменной meat присваивается в методе main: */
+                                                               /* вызываем метод show у gav */
+
+
+        System.out.println("There are black's food.");
+        Cat black = new Cat();                               /* Объект класса Gav black */
+        black.eat("fish");
+        black.giveNick("Black");
+        black.show();                                        /* вызываем метод show у black*/
+    }
+    }
---
+++ src/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+
+    public String engToRus(String eng) {
+        String voice = "неизветное слово" + eng;
+        System.out.println(voice);
+        return voice;
+
+    }
+}
+
+
