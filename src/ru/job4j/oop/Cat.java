323589a330b37c4ca20427c24b0462f99f0a7f6a
User <iuheniomail@yandex.ru>
2021-04-06 15:23:44 +0300

    1.5. Поля объекта[166083#272439]

--- src/ru/job4j/oop/Cat.java
+++ src/ru/job4j/oop/Cat.java
@@ -12,7 +12,7 @@ public class Cat {
 
     public void eat(String meat) {    /* записываем в поле объекта данные */
         this.food = meat;             /* Выше приведенная строчка записывает в поле объект food значение переменной
-                                            meat. Значение переменной meat присваивается в методе main: */
+                                          meat. Значение переменной meat присваивается в методе main: */
     }
     public void giveNick(String nick) {    /* 2 */
         this.name = nick;
@@ -26,11 +26,10 @@ public class Cat {
         gav.show();                                                    /*   Значение переменной meat присваивается в методе main: */
                                                                /* вызываем метод show у gav */
 
-
-        System.out.println("There are black's food.");
-        Cat black = new Cat();                               /* Объект класса Gav black */
-        black.eat("fish");
+        Cat black = new Cat();                                 /* Объект класса Gav black */
         black.giveNick("Black");
+        System.out.print("съел ");
+        black.eat("fish");
         black.show();                                        /* вызываем метод show у black*/
     }
     }
