fbac8c8a015001724c9cec9c17ae295c753e92ff
User <iuheniomail@yandex.ru>
2021-04-07 18:48:23 +0300

    1.2. Вызов метода объекта[166085#272440]

--- src/ru/job4j/oop/Student.java
+++ src/ru/job4j/oop/Student.java
@@ -1,15 +1,12 @@
 package ru.job4j.oop;
 
 public class Student {
-
     public void music() {
         System.out.println("Tra tra tra");
     }
     public void song() {
         System.out.println("I believe I can fly");
     }
-
-
     public static void main(String[] args) {
         Student petya = new Student();
         petya.music();
@@ -18,6 +15,5 @@ public class Student {
         petya.song();
         petya.song();
         petya.song();
-
     }
 }
