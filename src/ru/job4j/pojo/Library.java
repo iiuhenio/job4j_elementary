04dc9ed44d9bff828b63c2c30620d252b340028f
User <iuheniomail@yandex.ru>
2021-05-19 17:52:51 +0300

    3.1. Массив с пустыми ячейками [#176470]

--- src/ru/job4j/pojo/Library.java
+++ src/ru/job4j/pojo/Library.java
@@ -5,27 +5,32 @@ public class Library {
         Book pinokio = new Book("Pinokio", 4);
         Book mars = new Book("Mars", 15);
         Book jorney = new Book("Jorney", 27);
-        Book billionare = new Book("Billionare", 41);
-        Book cleancode = new Book("Clean code", 20);
+        Book cleancode = new Book("Clean code", 41);
+
         Book[] bookshelf = new Book[4];
         bookshelf[0] = pinokio;
         bookshelf[1] = mars;
         bookshelf[2] = jorney;
-        bookshelf[3] = billionare;
+        bookshelf[3] = cleancode;
         for (int index = 0; index < bookshelf.length; index++) {
             Book bo = bookshelf[index];
             System.out.println(bo.getName() + " - " + bo.getCount());
         }
         System.out.println("replace Pinokio to Billionare");
-        bookshelf[0] = billionare;
-        bookshelf[3] = pinokio;
         for (int index = 0; index < bookshelf.length; index++) {
+            Book temp = bookshelf[0];
+            bookshelf[0] = bookshelf[3];
+            bookshelf[3] = temp;
+
+
             System.out.println(bookshelf[index].getName() + " - " + bookshelf[index].getCount());
         }
         System.out.println("show Clean code book");
         for (int index = 0; index < bookshelf.length; index++) {
-            bookshelf[0] = cleancode;
-            System.out.println(bookshelf[index].getName() + " - " + bookshelf[index].getCount());
+            if (bookshelf[index].getName().equals("Clean code")) {
+
+                System.out.println(bookshelf[index].getName() + " - " + bookshelf[index].getCount());
+            }
         }
     }
 }
