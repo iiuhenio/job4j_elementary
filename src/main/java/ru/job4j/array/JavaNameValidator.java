a45f76d2351ff18f047859e51ce6d80767940891
iiuhenio <iuheniomail@yandex.ru>
2023-01-16 23:34:07 +0300

    6.9. String - это массив [#504884 #373198]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -9,16 +9,17 @@ public class JavaNameValidator {
      * Переменная должна начинаться на строчную латинскую букву.
      */
     public static boolean isNameValid(String name) {
-        boolean valid = name.length() != 0;
-        for (int i = 0; i < name.length(); i++) {
-            int code = name.codePointAt(i);
-            if (!JavaNameValidator.isLowerLatinLetter(name.codePointAt(0))) {
-                valid = false;
-                break;
-            }
-            if (JavaNameValidator.isSpecialSymbol(code) || JavaNameValidator.isUpperLatinLetter(code)
-                    || JavaNameValidator.isLowerLatinLetter(code)) {
-                valid = true;
+        boolean valid = !name.isEmpty() && isLowerLatinLetter(name.codePointAt(0));
+        if (valid) {
+            for (int i = 1; i < name.length(); i++) {
+                int code = name.codePointAt(i);
+                if (!isSpecialSymbol(code)
+                        && !isUpperLatinLetter(code)
+                        && !isLowerLatinLetter(code)
+                        && !Character.isDigit(code)) {
+                    valid = false;
+                    break;
+                }
             }
         }
         return valid;
@@ -35,14 +36,14 @@ public class JavaNameValidator {
      * метод должен вернуть true, если символ является заглавным латинским символом
      */
     public static boolean isUpperLatinLetter(int code) {
-        return code > 65 && code < 90;
+        return code >= 65 && code <= 90;
     }
 
     /**
      * метод должен вернуть true, если символ является маленьким латинским символом
      */
     public static boolean isLowerLatinLetter(int code) {
-        return code > 97 && code < 122;
+        return code >= 97 && code <= 122;
     }
 
     public static void main(String[] args) {
