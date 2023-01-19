986a2be82f47a3dbbf734887de57e45126ecf5ed
iiuhenio <iuheniomail@yandex.ru>
2023-01-19 14:01:34 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #382569]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -4,6 +4,11 @@ public class PasswordValidator {
 
     public static String validate(String password) {
 
+        int digit = 0;
+        int special = 0;
+        int upCount = 0;
+        int loCount = 0;
+
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
@@ -12,41 +17,42 @@ public class PasswordValidator {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
 
-        String str = "qwerty";
-        String str1 = "12345";
-        String str2 = "password";
-        String str3 = "admin";
-        String str4 = "user";
-        if (password.toLowerCase().contains(str) || password.toLowerCase().contains(str1)
-        || password.toLowerCase().contains(str2) || password.toLowerCase().contains(str3)
-        || password.toLowerCase().contains(str4)) {
-            throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345,"
-                    + " password, admin, user");
+        String[] strings = new String[] {"qwerty", "12345", "password", "admin", "user"};
+        for (String string : strings) {
+            if (password.toLowerCase().contains(string)) {
+                throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345,"
+                        + " password, admin, user");
+            }
         }
 
         for (int i = 0; i < password.length(); i++) {
-            int code = password.codePointAt(i);
-            if (!isLowerLatinLetter(code)) {
-                return password;
+            char ch = password.charAt(i);
+            if (Character.isDigit(ch)) {
+                digit++;
             }
-            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
+            if (Character.isUpperCase(ch)) {
+                upCount++;
+            }
+            if (Character.isLowerCase(ch)) {
+                loCount++;
+            }
+            if (!Character.isLetterOrDigit(ch)) {
+                special++;
+            }
         }
+
+            if (digit == 0) {
+                throw new IllegalArgumentException("Password should contain at least one figure");
+            }
+            if (upCount == 0) {
+                throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+            }
+            if (loCount == 0) {
+                throw new IllegalArgumentException("Password should contain at least one lowercase letter");
+            }
+            if (special == 0) {
+                throw new IllegalArgumentException("Password should contain at least one special symbol");
+            }
         return password;
-    }
-
-    public static boolean isUpperLatinLetter(int code) {
-        return code >= 65 && code <= 90;
-    }
-
-    public static boolean isLowerLatinLetter(int code) {
-        return code >= 97 && code <= 122;
-    }
-
-    public static boolean isNumber(int code) {
-        return code >= 48 && code <= 57;
-    }
-
-    public static boolean isSpecialSymbol(int code) {
-        return !isUpperLatinLetter(code) && !isLowerLatinLetter(code) && !isNumber(code);
     }
 }
